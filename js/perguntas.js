be74a835447ddae6fa6219d5c0cc084b23511fb3
Matheus Fellipe Turesso <103067397+zTuressOz@users.noreply.github.com>
2024-09-18 10:33:06 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,83 @@
+export const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, uma segurança digital que consegue cuidar de todas as suas senhas, ele também é capaz de guardar seus emails e suas senhas mais pessoais em qualquer aplicativo. Qual seu primeiro pensamento sobre?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [
+                    "No início ficou com medo do que essa tecnologia pode fazer." ,
+                    "No começo se sentiu apreensivo sobre deixar dados tão importantes nas mãos de um software."
+                ]
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [
+                "No início quis saber como usar IA no seu dia a dia.",
+                "No começo ficou fascinado com os serviços que uma IA poderia fazer. "
+            ]
+                
+            }
+        ]
+    },
+    {
+        enunciado: "Com a descoberta desta IA, chamada Segurança Digital, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre este recurso novo. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA na segurança digital em sala de aula. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Utiliza uma de suas contas pessoais para servir como teste para mostrar como eles guardam a senha com segurança.",
+                afirmacao: [
+                    "Conseguiu utilizar a IA de segurança digital para guardar suas senhas.",
+                    "Realizou testes para conhecer mais sobre a IA. "
+            ]
+            },
+            {
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: ["Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
+                "Achou que fosse melhor procurar informações sobre em outros lugares do que se arriscar em um teste."
+            ]
+            }
+        ]
+    },
+    {
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de segurança sobre a internet e melhorar no cuidado com as contas.",
+                afirmacao: ["Vem impulsionando a inovação na área Segurança da Internet e luta para abrir novos caminhos com a IA.",
+                "Acredita num futuro técnológico com avanços na área da segurança digital e está certo que a IA irá abrir novos caminhos."]
+            },
+            {
+                texto: "Me preocupo com as pessoas que poderão perder suas contas para caso essa segurança digital falhe",
+                afirmacao: ["Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                "Você não confia na capacidade da IA de nos proteger na internet e acredita que devemos tomar precauções antes de usar esses recursos tão avançados."]
+            }
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou mostrar como a segurança digital vai ajudar você a guardar suas senhas sem que outras pessoas roubem. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma conta e tentar entrar nessa conta por um computador diferente.",
+                afirmacao: ["Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos sobre criar email utilizando ferramentas para iniciantes.",
+                            "Decidiu compartilhar esse conhecimento com as outras pessoas já que é importante espalhar as novas tecnolgias para o mundo"
+            ]
+            },
+            {
+                texto: "Criar uma conta e tentar entrar nessa conta pelo gmail sem utilizar da senha.",
+                afirmacao: "Acelerou o processo de criação de email utilizando o recurso básico e agora consegue ensinar pessoas que sentem dificuldades em entrar em contas mais facilmente, ajudandpo como utilizar também!"
+            }
+        ]
+    },
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo perdeu a senha da conta dela. O problema é que a base do trabalho esta totalmente nessa conta. O que você faz? ",
+        alternativas: [
+            {
+                texto: "Tento utilizar a segurança digital para recuperar minha senha com facilidade.",
+                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+            },
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso guardar a senha e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+            }
+        ]
+    },
+];
